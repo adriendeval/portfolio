ba6af2382c0896d5c0a89a0c458f1ab31bc3e536
adriendeval <adrien.deval@ikmail.com>
2025-12-03 14:17:37 +0100

    add certifications and mentions légales

---
+++ src/Controller/CertificationsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CertificationsController extends AbstractController
+{
+    #[Route('/certifications', name: 'app_certifications')]
+    public function index(): Response
+    {
+        return $this->render('certifications/index.html.twig', [
+            'controller_name' => 'CertificationsController',
+        ]);
+    }
+}
