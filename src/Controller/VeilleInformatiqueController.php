9421999b755c59b6381b2eb468d17ae3f0619586
adriendeval <adrien.deval@ikmail.com>
2025-12-03 13:25:09 +0100

    design v2

--- src/Controller/VeilleInformatiqueController.php
+++ src/Controller/VeilleInformatiqueController.php
@@ -11,8 +11,44 @@ final class VeilleInformatiqueController extends AbstractController
     #[Route('/veille-informatique', name: 'app_veille_informatique')]
     public function index(): Response
     {
+        $rssUrl = 'https://www.actuia.com/feed';
+        $rssContent = file_get_contents($rssUrl);
+        // Force UTF-8 encoding
+        // $rssContent = mb_convert_encoding($rssContent, 'UTF-8', 'ISO-8859-1');
+        $rss = @simplexml_load_string($rssContent);
+        
+        $articles = [];
+
+        if ($rss) {
+            $formatter = new \IntlDateFormatter(
+                'fr_FR',
+                \IntlDateFormatter::FULL,
+                \IntlDateFormatter::NONE,
+                'Europe/Paris',
+                \IntlDateFormatter::GREGORIAN,
+                'eeee d MMMM y'
+            );
+
+            $count = 0;
+            foreach ($rss->channel->item as $item) {
+                if ($count >= 5) {
+                    break;
+                }
+
+                $date = new \DateTime((string) $item->pubDate);
+
+                $articles[] = [
+                    'title' => (string) $item->title,
+                    'link' => (string) $item->link,
+                    'description' => html_entity_decode((string) $item->description),
+                    'pubDate' => ucfirst($formatter->format($date)),
+                ];
+                $count++;
+            }
+        }
+
         return $this->render('veille_informatique/index.html.twig', [
-            'controller_name' => 'VeilleInformatiqueController',
+            'articles' => $articles,
         ]);
     }
 }
